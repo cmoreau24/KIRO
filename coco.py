9f57bf6446be7cade102c01a113c67d42e186849
Corentin Moreau <moreaucorentin02@gmail.com>
2024-11-14 21:19:04 +0100

    vf

---
+++ coco.py
@@ -0,0 +1,36 @@
+from numpy import random
+import pandas as pd
+import json
+
+L = [random.randint(1,5) for i in range(random.randint(10))]
+print(L)
+
+with open('tiny.json', 'r') as file:
+    data = json.load(file)
+
+
+
+vehicles = {}
+for i in range(len(data["vehicles"])):
+    vehicles[i] = data["vehicles"][i]["type"]
+
+#print(vehicles)
+
+
+constraints = {}
+for i in range(len(data["constraints"])):
+    constraints[i] = data["constraints"][i]
+
+#print(constraints)
+
+
+def women_be_shopping(L):
+    cout=0
+    for shop in constraints:
+        if shop["shop"] == "paint":
+            L_entry = L
+            L_exit = []
+            for i in range(len(L_entry)):
+                if L_entry[i]
+                    L_exit.append()
+        
